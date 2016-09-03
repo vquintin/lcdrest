ea457066814e1f50f9772461422e2f7b19b6d6aa
Virgile Quintin <virgile.quintin@gmail.com>
2016-09-03 02:13:11 +0200

    WIP

--- lcdrest/lcdwrapper.go
+++ lcdrest/lcdwrapper.go
@@ -1,51 +1,52 @@
-package lcdrest
+package main
 
-import 	"github.com/vquintin/i2c"
+import "github.com/vquintin/i2c"
 
 const lineSize = 20
 const screenSize = 4 * lineSize
 
 type lcdWrapper struct {
-    lcd *i2c.Lcd
+	lcd *i2c.Lcd
 }
 
 func (lw *lcdWrapper) Write(buf []byte) (int, error) {
-    lcd.Clear()
-    lcd.Home()
-    buffer := buf[:screenSize]
-    buffer = reorderLines(buffer)
-    return lcd.Write(buffer)
+	lw.lcd.Clear()
+	lw.lcd.Home()
+	buffer := buf[:screenSize]
+	buffer = reorderLines(buffer)
+	return lw.lcd.Write(buffer)
 }
 
 func reorderLines(buffer []byte) []byte {
-    size := bufferSize(len(buffer))
-    result := make([]byte, size)
-    for i,_ := range result {
-        result[i] = ' '
-    }
-    for i, v := range buffer {
-        result[position(i)] = buffer[i]
-    }
+	size := bufferSize(len(buffer))
+	result := make([]byte, size)
+	for i, _ := range result {
+		result[i] = ' '
+	}
+	for i, v := range buffer {
+		result[position(i)] = buffer[i]
+	}
+	return result
 }
 
 func bufferSize(n int) int {
-    if n < lineSize {
-        return n
-    } else if n < 2 * lineSize {
-        n + lineSize
-    } else if n < 3 * lineSize {
-        return 3 * lineSize
-    }
-    return n
+	if n < lineSize {
+		return n
+	} else if n < 2*lineSize {
+		return n + lineSize
+	} else if n < 3*lineSize {
+		return 3 * lineSize
+	}
+	return n
 }
 
 func position(i int) int {
-    if i < lineSize {
-        return n
-    } else if i < 2 * lineSize {
-        return n + lineSize
-    } else if n < 3 * lineSize {
-        return n - lineSize
-    }
-    return n
+	if i < lineSize {
+		return i
+	} else if i < 2*lineSize {
+		return i + lineSize
+	} else if i < 3*lineSize {
+		return i - lineSize
+	}
+	return i
 }
